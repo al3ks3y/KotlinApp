dc60b309a2ec3efa22937ae41b04c536f861de7a
alexeypetrov <lomon2008@yandex.ru>
2020-08-08 13:05:10 +0300

    Added doctor class

---
+++ src/main/kotlin/com/apetrov/kotlinrestapp/service/DoctorService.kt
@@ -0,0 +1,12 @@
+package com.apetrov.kotlinrestapp.service
+
+import com.apetrov.kotlinrestapp.dao.DoctorDao
+import com.apetrov.kotlinrestapp.model.Doctor
+import org.springframework.stereotype.Service
+
+@Service
+class DoctorService (val doctorDao: DoctorDao){
+    fun getAll():List<Doctor>{
+       return doctorDao.findAll()
+    }
+}
