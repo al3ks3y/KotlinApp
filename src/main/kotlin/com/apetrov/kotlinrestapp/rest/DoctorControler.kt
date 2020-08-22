e86682ef33a37dcb2095559a6712ca70990cd8bd
alexeypetrov <lomon2008@yandex.ru>
2020-08-22 18:16:05 +0300

    Controllers update

--- src/main/kotlin/com/apetrov/kotlinrestapp/rest/DoctorControler.kt
+++ src/main/kotlin/com/apetrov/kotlinrestapp/rest/DoctorControler.kt
@@ -4,6 +4,7 @@ import com.apetrov.kotlinrestapp.model.Doctor
 import com.apetrov.kotlinrestapp.rest.outdto.DoctorOutDto
 import com.apetrov.kotlinrestapp.service.DoctorService
 import io.swagger.annotations.ApiOperation
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/doctor")
 class DoctorControler (val doctorService: DoctorService){
     @ApiOperation("Получить список всех врачей в базе")
+    @GetMapping
     fun getAllDoctors(): List<DoctorOutDto> {
         return doctorService.getAll()
     }
--- src/main/kotlin/com/apetrov/kotlinrestapp/rest/ServiceController.kt
+++ src/main/kotlin/com/apetrov/kotlinrestapp/rest/ServiceController.kt
@@ -1,10 +1,17 @@
 package com.apetrov.kotlinrestapp.rest
 
+import com.apetrov.kotlinrestapp.rest.indto.DoctorInDto
+import com.apetrov.kotlinrestapp.service.AdminService
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/admin")
-class ServiceController {
+class ServiceController (val adminService: AdminService){
+    @PostMapping
+    fun addDoctor(doctorInDto: DoctorInDto){
+        adminService.addDoctor(doctorInDto)
+    }
 
 }
