53fe987a87d8fef77c844740a9cf0728ce1a5058
alexeypetrov <lomon2008@yandex.ru>
2020-08-22 18:11:09 +0300

    security config

--- src/main/kotlin/com/apetrov/kotlinrestapp/KotlinRestappApplication.kt
+++ src/main/kotlin/com/apetrov/kotlinrestapp/KotlinRestappApplication.kt
@@ -1,10 +1,12 @@
 package com.apetrov.kotlinrestapp
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
+@EnableAutoConfiguration
 class KotlinRestappApplication
 
 fun main(args: Array<String>) {
