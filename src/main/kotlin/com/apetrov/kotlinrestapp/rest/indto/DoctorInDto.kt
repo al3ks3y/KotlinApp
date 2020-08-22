270acc1942048dcd83da706c9c3b90753a5b5d8a
alexeypetrov <lomon2008@yandex.ru>
2020-08-22 18:20:39 +0300

    Date format updated

--- src/main/kotlin/com/apetrov/kotlinrestapp/rest/indto/DoctorInDto.kt
+++ src/main/kotlin/com/apetrov/kotlinrestapp/rest/indto/DoctorInDto.kt
@@ -1,12 +1,14 @@
 package com.apetrov.kotlinrestapp.rest.indto
 
 import com.apetrov.kotlinrestapp.enums.Degree
+import org.springframework.format.annotation.DateTimeFormat
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class DoctorInDto(
         val name:String,
         val surname:String,
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
         val birthday:LocalDate,
         val salary:BigDecimal,
         val careerStartYear:Int,
